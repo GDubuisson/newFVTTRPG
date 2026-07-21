fe67857a98a6bf68d605bd9f4019681b1edf4b68
GDubuisson <dubuisson.guillaume@gmail.com>
2026-07-21 18:32:34 +0200

    Premiere commande pour systeme D&D

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,22 @@
 // Main JavaScript file for the New FVTTRPG System
 Hooks.once('init', async function() {
   console.log('Initializing New FVTTRPG System');
+
+  // Ajout des types d'entités pour D&D
+  CONFIG.Actor.documentClass = DnDActor;
+  CONFIG.Item.documentClass = DnDItem;
 });
 
+class DnDActor extends Actor {
+  prepareData() {
+    super.prepareData();
+    // Ajout des données spécifiques aux acteurs D&D
+  }
+}
+
+class DnDItem extends Item {
+  prepareData() {
+    super.prepareData();
+    // Ajout des données spécifiques aux objets D&D
+  }
+}
